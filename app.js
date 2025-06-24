9fb5e8b06c5088ba9552bb24515133ea33dea184
LeilaSmh <leilasamih.ls@gmail.com>
2025-06-24 11:38:38 +0100

    "special attack" functionality

--- app.js
+++ app.js
@@ -7,18 +7,40 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0,
         };
     },
-    methods: {
-        attackMonster() {
-            const damage = getRandomInt(5, 12);
-            this.monsterHealth -= damage;
-            this.attackPlayer();
+    computed: {
+        monsterBarStyles() {
+            return { width: this.monsterHealth + '%' };
         },
+        playerBarStyles() {
+            return { width: this.playerHealth + '%' };
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 === 0 && this.currentRound > 0;
+        },
+    },
+    methods: {
         attackPlayer() {
             const damage = getRandomInt(8, 15);
             this.playerHealth -= damage;
         },
+        attackMonster() {
+            const damage = getRandomInt(5, 12);
+            this.monsterHealth -= damage;
+            this.attackPlayer();
+            this.increaseRound();
+        },
+        specialAttackMonster() {
+            const damage = getRandomInt(10, 25);
+            this.monsterHealth -= damage;
+            this.attackPlayer();
+            this.increaseRound();
+        },
+        increaseRound() {
+            this.currentRound++;
+        }
     }
 });
 
